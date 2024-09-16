7f432dc2e72a89b56bd651110dfb7a90a2de36d6
philemon <mandelap21@gmail.com>
2024-09-16 15:36:55 +0300

    Route lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import Card from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  const [actors, setActors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const actorList = actors.map(actor => <Card key={actor.id} name={actor.name} movies={actor.movies} />)
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actors.map((actor, index) => (
+        <article key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies?.map((movie, index) => (
+              <li key={index}>{movie.title}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
       </main>
     </>
   );
-};
+}
 
 export default Actors;
